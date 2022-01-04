952236c2d0f95f234882bba3569163723241978a
bsao17 <declic62@gmail.com>
2022-01-04 12:08:06 +0100

    init serverlessChat

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { getDatabase, ref, set, onValue, get } from "firebase/database";
+import { app } from "./firebaseApp";
+
+const database = getDatabase(app);
+const databseRef = ref(database, "users/");
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [dataDb, setDataDb] = useState([]);
+
+  function writeUserData(name, email, imageUrl) {
+    // database state
+    setDataDb({ name, email, imageUrl });
+    // database write
+    set(databseRef, {
+      username: name,
+      email: email,
+      profile_picture: imageUrl,
+    });
+    setLoading(false);
+  }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +32,20 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          onClick={() => {
+            writeUserData("Simba", "declic62@gmail.com", "image/de/bruno.jpg");
+            console.log(database);
+          }}
         >
-          Learn React
-        </a>
+          Write
+        </button>
+        <label>
+          Name:
+          {loading ? <span>Loading...</span> : <span>{
+            dataDb.name
+            }</span>}
+        </label>
       </header>
     </div>
   );
