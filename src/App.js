15571103e695e118b4b205995cd0f9bf756f4ad2
bsao17 <declic62@gmail.com>
2022-01-04 20:19:40 +0100

    add a little bit more style

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
 import React from "react";
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"
+import {initializeApp} from "firebase/app";
+import {getFirestore} from "firebase/firestore"
 import {collection, addDoc} from "firebase/firestore";
 import {firebaseConfig} from "./firebase/firebaseConfig";
-const firebaseApp = initializeApp(firebaseConfig)
 
+// only in case of connection breakage
+const firebaseApp = initializeApp(firebaseConfig)
 const db = getFirestore();
 
+// Style title
+const title = {
+    color: "white",
+    fontWeight: "900"
+}
+
 export default function App() {
-    async function writeData() {
+    // database Storage
+    async function writeData(first, last, born) {
         try {
             const docRef = await addDoc(collection(db, "users"), {
-                first: "Ada",
-                last: "Lovelace",
-                born: 1815
+                first: first,
+                last: last,
+                born: born
             });
             console.log("Document written with ID: ", docRef.id);
         } catch (e) {
             console.error("Error adding document: ", e);
         }
     }
+
     return (
-        <div>
-            <button onClick={ ()=> writeData()}>Send Firestore</button>
+        <div style={{
+            width: "100%",
+            height: "100vh",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#508bb0"
+        }}>
+            <h1 style={title}>Using Google Firebase Firestore</h1>
+            <button
+                onClick={() => writeData("enzo", "Mehddeb", 2005)}
+                style={{
+                    width: "25%",
+                    backgroundColor: "blue",
+                    borderRadius: "15px",
+                    color: "white",
+                    fontSize: "1.2rem"
+                }}
+            >Send Firestore
+            </button>
         </div>
     )
 }
